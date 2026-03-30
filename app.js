5678c4254be4cc3ee3b348509a9b1cc04f5bded7
HENRY <henry.nwokolo23@northampton.ac.uk>
2026-03-30 08:37:01 +0100

    added note objects with properties

--- app.js
+++ app.js
@@ -14,3 +14,17 @@ function loadSection(currentSection) {
 const sections = ["Notes", "Articles", "Literature" , "Poetry", "journal" ]
 
 sections.forEach(loadSection)
+
+const notes = {
+    title: "my first note",
+    content : "writing the most rando stuff on here ",
+    section: "Notes",
+    tone : "casual",
+    wordCount: 10
+}
+
+console.log(notes.title)
+console.log(notes.content)
+console.log(notes.section)
+console.log(notes.tone)
+console.log(notes.wordCount)
